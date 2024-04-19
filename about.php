82aa07ef780774586d0c0416e4c3f28a1fc659e4
bernaselomen <162945684+bernaselomen@users.noreply.github.com>
2024-04-18 20:51:26 -0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+  <head>
+
+    <meta charset="UTF-8" />
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap"
+
+      rel="stylesheet"
+
+    />
+
+    <link rel="stylesheet" href="../style.css" />
+
+
+</head>
+
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+
+
+
+</head>
+<body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+<style>
+{
+  margin-left:20px;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.topnav {
+  overflow: hidden;
+  background-color: violet;
+  margin-left:1000px;
+}
+
+.topnav a {
+  float: left;
+  color: black;
+  text-align: center;
+  padding: 10px 20px;
+  text-decoration: none;
+  font-size: 17px;
+  ash-color:yellow;
+}
+
+.topnav a:hover {
+  background-color: green;
+  color: red;
+  
+}
+
+.topnav a.active {
+  background-color: #04AA6D;
+  color: white;
+ 
+}
+
+</style>
+</head>
+<body>
+<div class="topnav">
+  
+  
+<a class="active" href="index.php">Home</a>
+  <a href="about.php">About</a>
+  <a href="contact.php">Contact</a>
+  <a href="log_in.php">Log_in</a>
+</div>
+
+
+
+</body>
+</html>
+
+
+
+</body>
+</html>
+<nav class="navbar navbar-light justify-content-center fs-3 mb-5" style="background-color:#FF80AA;">
+
+    <nav>
+      <div class="heading">
+      </div>
+
+      <ul class="nav-links">
+
+
+      </ul>
+
+    </nav>
+  <center>
+    <h1>  Welcome  DTC!</h1>
+  <div class="body-text"><h1> BACHELOR OF SCIENCE IN INFORMATION SYSTEM</h1></div>
+
+    <div> 
+    <img src="download.jfif" style="float:left" height="600" width="500" />
+ </div> 
+
+ </center>
+
+   
+   
+    <center>
+<p>The Bachelor of Science in Information Systems (BSIS) program is a four-year degree program which focuses on the design and implementation of computer-based information systems and other ICT solutions to address the demands in existing business processes.
+
+The program prepares students to become IT professionals with primary competencies in the areas of business process analysis and reengineering, change management, systems integration, systems implementation, system evaluation/quality assurance, and software maintenance.
+
+The program also requires a Capstone project. It should be in the form of an information systems development and implementation as a business solution for an industry need. 
+
+
+
+
+
+ <h2>OBJECTIVES OF THE PROGRAM </h2><br
+<p>• To introduce students to current technologies and tools, as well as, quality assurance methodologies that will lead to the development of implementation-ready information systems.
+<p>.To enable students to understand the different components of the information technology field, including hardware, software, communication, networking, research, peopleware and management skills.
+<p>.To demonstrate awareness of how to methodically and practically approach a variety of technical, organizational, and managerial issues to ultimately improve ICT integration towards the attainment of strategic business advantage.
+<p>.To inculcate to students the essential virtues and attitudes, as well as develop necessary knowledge and competency levels required of an information technology professional.
+<p>.To train students to systematically appraise the quality of computer-based information systems against user needs and requirements to guarantee user acceptance and implement ability.
+
+
+
+</center>		 
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  </body>
+
+</html>
+
---
+++ contact.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> Gera</title>
+<link rel="stylesheet"href="putot.css">
+<script src="login.js"></script>
+
+<style>
+{
+  margin-left:20px;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.topnav {
+  overflow: hidden;
+  background-color: green;
+  margin-left:1000px;
+}
+
+.topnav a {
+  float: left;
+  color: black;
+  text-align: center;
+  padding: 10px 20px;
+  text-decoration: none;
+  font-size: 17px;
+  ash-color:pink;
+}
+
+.topnav a:hover {
+  background-color: light green;
+  color: red;
+  
+}
+
+.topnav a.active {
+  background-color: #04AA6D;
+  color:;pink
+
+
+ 
+}
+.h1{
+  color:blue;
+
+}
+
+</style>
+</head>
+<body>
+<div class="topnav">
+<a class="active" href="index.php">Home</a>
+  <a href="about.php">About</a>
+  <a href="contact.php">Contact</a>
+  <a href="log_in.php">Log_in</a>
+  
+</div>
+
+
+
+</body>
+</html>
+
+
+</style>
+<body>
+<nav class="navbar navbar-light justify-content-center fs-3 mb-5" style="background-color:#FF80AA;">
+
+<nav class="navbar navbar-light justify-content-center fs-10 mb-5" style="background-color:BFBFBF;">
+
+	<div> 
+    <img src="ge.jpg" style="float:left" height="100" width="200" />
+ </div>
+	   <h1>Name: </h1>
+	   <h1> Geralyn Macalandag</h1>
+   </div>
+   <td>
+   <div> 
+    <img src="ADDRESS.png" style="float:left" height="100" width="200" />
+ </div> 
+	   <h1>ADDRESS:</h1>
+	   <h1> Tabok, Danao, Bohol</h1>
+</div>
+</td>
+<td>
+<img src="PIC.jfif" style="float:left" height="100" width="200" />
+ </div> 
+	   <h1>PHONENUMBER:</h1>
+	   <h1> 09104885161</h1>
+</div>
+</td>
+</td>
+<div> 
+    <img src="EMAIL.jpg" style="float:left" height="100" width="200" />
+ </div>  
+	   <h1>EMAIL:</h1>
+	   <h1>geralynmacalandag605@gmail.com</h1>
+
+
+
+
+</div>
+</td>
+	   </center>
+   </div>
+   </tr>
+</body>
+</html>
